8b0171bf724bb567abe2633469c9072e47948bcd
Jakub Kasztura <jakub.kasztura@gmail.com>
2023-06-02 22:43:13 +0200

    Progress bar follow time

--- 11 Custom Video Player/app.js
+++ 11 Custom Video Player/app.js
@@ -9,15 +9,18 @@ const player = document.querySelector(".player"),
   forwardButton = player.querySelector("button[data-skip='25']");
 
 let videoStatus = false;
+let intervalId;
 const startAndStopPlaying = function () {
   if (!videoStatus) {
     video.play();
     playButton.innerHTML = "&#10074&#10074";
     videoStatus = true;
+    intervalId = setInterval(changeProgressBar, 200);
   } else {
     video.pause();
     playButton.innerHTML = "►";
     videoStatus = false;
+    clearInterval(intervalId);
   }
 };
 const changeVolume = function () {
@@ -35,6 +38,11 @@ const backward10s = function () {
 const forward25s = function () {
   let seconds = 25;
   video.currentTime += seconds;
+};
+
+const changeProgressBar = function () {
+  let currentTime = video.currentTime;
+  progressFilled.style.flexBasis = `${currentTime}%`;
 };
 
 const initial = function () {
